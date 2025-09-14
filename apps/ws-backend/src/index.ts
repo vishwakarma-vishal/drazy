f90ef0573335adf4fdb4e4044c05c0dee18c819d
vishwakarma-vishal <vishal.vishwakarma.dev@gmail.com>
2025-09-15 01:24:29 +0530

    created an in memory state variable with different method for different use like addUser, joinRoom, leaveRoom etc.

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,20 +1,68 @@
 import { WebSocketServer } from "ws";
 import dotenv from "dotenv";
-
 dotenv.config({ path: "../../.env" });
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { addUser, brodcastMessage, joinRoom } from "./state";
 
 const WS_PORT = process.env.WS_PORT;
-
 const wss = new WebSocketServer({ port: Number(WS_PORT) });
 
 wss.on("listening", () => {
     console.log("ws server is running...");
 });
 
-wss.on("connection", (ws) => {
+wss.on("connection", (ws, request) => {
     ws.on("error", console.error);
 
+    // only authorized user can join the ws server
+    const url = new URL(request.url || "", `http://${request.headers.host}`);
+    const token = url.searchParams.get("token");
+
+    if (!token) {
+        ws.close(4001, "Missing token");
+        return;
+    }
+
+    const userId = validateUser(token);
+    if (!userId) {
+        ws.close(4002, "Invalid or expired token");
+        return;
+    }
+
+    // join web socket server (add in state)
+    addUser(userId, ws);
+
     ws.on("message", (data) => {
-        ws.send("pong");
+        const parsedData = JSON.parse(data.toString());
+        
+        console.log("parsed ", parsedData);
+
+        if (parsedData.type === "join") {
+            joinRoom(userId, parsedData.roomId);
+        }
+
+        if (parsedData.type === "chat") {
+            brodcastMessage(parsedData.roomId, parsedData.message)
+        }
     })
 });
+
+interface MyJwtToken extends JwtPayload {
+  data?: {
+    id: string;
+  };
+}
+
+const validateUser = (token: string) => {
+    try {
+        const JWT_SECRET = process.env.JWT_SECRET;
+
+        if (!JWT_SECRET) throw new Error("JWT_SECRET is not defined in environment variables");
+
+        const decode = jwt.verify(token, JWT_SECRET) as MyJwtToken;
+        return decode.data?.id;
+    } catch(error) {
+        console.log("Jwt vefification failed with error ->", error);
+        return null;
+    }
+}
---
+++ apps/ws-backend/src/state.ts
@@ -0,0 +1,71 @@
+// state managment
+interface UserI {
+    userId: string,
+    ws: WebSocket,
+    rooms: Set<string>
+}
+
+// userId → UserI
+const users = new Map<string, UserI>();
+// roomId → Set<userId>
+const rooms = new Map<string, Set<string>>();
+
+const addUser = (userId: string, ws: WebSocket) => {
+    users.set(userId, {
+        userId,
+        ws,
+        rooms: new Set()
+    })
+}
+
+const joinRoom = (userId: string, roomId: string) => {
+    const currUser = users.get(userId);
+    if (!currUser) return;
+    // add room to user
+    currUser.rooms.add(roomId);
+
+    // add user to room
+    if (!rooms.has(roomId)) rooms.set(roomId, new Set());
+    rooms.get(roomId)?.add(userId);
+}
+
+const leaveRoom = (userId: string, roomId: string) => {
+    const user = users.get(userId);
+    if (!user) return;
+    user.rooms.delete(roomId);
+
+    const room = rooms.get(roomId);
+    room?.delete(userId);
+
+    // clean empty room
+    if (rooms.get(roomId)?.size === 0) {
+        rooms.delete(roomId);
+    }
+}
+
+const deleteUser = (userId: string) => {
+    const user = users.get(userId);
+    if (!user) return;
+
+    // remove from all rooms
+    for (const roomId of user.rooms) {
+        rooms.get(roomId)?.delete(userId);
+
+        if (rooms.get(roomId)?.size === 0) {
+            rooms.delete(roomId);
+        }
+    }
+
+    // delete user
+    users.delete(userId);
+}
+
+const brodcastMessage = (roomId: string, message: string) => {
+    const userIds = rooms.get(roomId);
+
+    for (const user in userIds) {
+        users.get(user)?.ws.send(message);
+    }
+}
+
+export { addUser, joinRoom, leaveRoom, deleteUser, brodcastMessage };
