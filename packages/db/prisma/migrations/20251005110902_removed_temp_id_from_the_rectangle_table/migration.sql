f9c0376d9c43e14c6f7f4e5e0d52ebbcea88eecd
vishwakarma-vishal <vishal.vishwakarma.dev@gmail.com>
2025-10-07 16:27:52 +0530

    Delete tempId form the rectangle shape.

---
+++ packages/db/prisma/migrations/20251005110902_removed_temp_id_from_the_rectangle_table/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `tempId` on the `Rectangle` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."Rectangle" DROP COLUMN "tempId";
